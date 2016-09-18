4d44c4fc50b7620f3c0dbc0ca987186b3c7dec53
vcello <matthew.john.twomey@gmail.com>
2016-09-18 11:41:22 -0700

    documenting responsibilties

---
+++ webclient/javascript/quizgame.js
@@ -0,0 +1,7 @@
+// main entry point for the quizgame client
+//
+// Responsibilities:
+// - collect the player name and other information
+// - request questions from cgi-bin/get-question.pl
+// - accept player answer input and verify correctness
+// - keep a running total of correct answers per game and per player
