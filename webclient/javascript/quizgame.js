f7440482c0944e2aff7c725b7c7f1c63df7d262a
vcello <matthew.john.twomey@gmail.com>
2016-09-18 15:14:30 -0700

    splitting out js calls

--- webclient/javascript/quizgame.js
+++ webclient/javascript/quizgame.js
@@ -5,3 +5,15 @@
 // - request questions from cgi-bin/get-question.pl
 // - accept player answer input and verify correctness
 // - keep a running total of correct answers per game and per player
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"/>
+
+var request = $.ajax({
+        type: 'GET',
+        url: '../../cgi-bin/get-configs.pl',
+      });
+request.done(function(res) {
+  $("#mybox").html(res);
+};
+request.fail(function() {
+  $("#mybox").html("did not work");
+};
