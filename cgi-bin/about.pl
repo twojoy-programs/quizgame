48ce4c94291d28a3cfcea5ad45f059cc2f85ad38
Tucker R. Twomey <tt.runscript@gmail.com>
2016-11-08 22:11:52 -0800

    Copied and adapted git-version.pl for about.pl

--- cgi-bin/about.pl
+++ cgi-bin/about.pl
@@ -34,3 +34,35 @@ use strict;
 use warnings;
 use utf8;
 use v5.14;
+
+use CGI::Carp qw(fatalsToBrowser);
+use HTML::Template;
+use File::ReadBackwards;
+use File::Spec::Functions qw(rel2abs);
+use File::Basename;
+
+sub tail
+{
+    my ($file, $num_lines) = @_;
+
+    my $bw = File::ReadBackwards->new($file) or die "Can't read '$file': $!";
+
+    my ($lines, $count);
+    while (defined(my $line = $bw->readline) && $num_lines > $count++) {
+        $lines .= $line;
+    }
+
+    $bw->close;
+    return $lines;
+}
+# tail() is from http://stackoverflow.com/a/29267540.
+
+sub gitversion()
+{
+  my $currentdir = dirname(rel2abs($0));
+  my $git_dir = "$currentdir/../.git";
+  my $lastcommit = tail("$git_dir/logs/HEAD", 1);
+  my @lastcommit_split = split(/ /, $lastcommit);
+  my $output = substr($lastcommit_split[1], 0, 6);
+  return $output;
+}
