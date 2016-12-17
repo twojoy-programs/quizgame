4aa123803f5cc2f08f2449dc0453ddbb0428d4b9
Tucker R. Twomey <tt.runscript@gmail.com>
2016-12-17 15:23:51 -0800

    Added in time since modification of the quizfile.

--- cgi-bin/get-question.pl
+++ cgi-bin/get-question.pl
@@ -96,7 +96,10 @@ $question{"answers"}->[2]  = $question{"answer2"};
 $question{"answers"}->[3]  = $question{"answer3"};
 delete $question{"answer0"};  delete $question{"answer1"};
 delete $question{"answer2"};  delete $question{"answer3"};
-$question{"qfiletime"}     = undef; # Undef for now.
+
+$question{"qfiletime"}     =
+stat(dirname(rel2abs($0)) . $configs{"quizfilepath"})[9];
+
 $question{"id"}            = $qnumber;
 $question{"session-id"}    = int(rand(65535));
 $question{"correctanswr"}  = "" if $configs{"srvsidescore"};
